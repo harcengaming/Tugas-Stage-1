0dcc55011a5de4e188b213d3b692de082b19dfc1
harcengaming <65104722+harcengaming@users.noreply.github.com>
2021-08-18 13:02:36 +0700

    Update Bootstrap

--- javascript.js
+++ javascript.js
@@ -41,62 +41,55 @@ function getDataIndonesia() {
 }
 
 //=============Cari===============
-// function getDataCari() {
+function getDataNegara() {
 
-//     let dataCari = document.querySelector("cari");
-//     if(submit){
-//             submit.addEventListener("click", swapper, false);
-//         }
+    // let button = document.querySelector("cari-kotak");
+    // button.addEventListener("click", (dataNegara) => {
+    //         let search = document.getElementById("cari-input").value;
+    //         console.log(search);
 
-//     let cari = document.getElementById("cari-input");
-//     console.log(cari);
-        
+        let dataNegara = "<tr><th>Provinsi</th><th>Positif</th><th>Sembuh</th><th>Meninggal</th></tr>";
+        //   let dataNegara = document.querySelector("data-container");
 
+        fetch('https://covid19.mathdro.id/api/confirmed/')
+        .then(response2 => response2.json())
+        .then(data2 => {
+            console.log(data2)
+            data2.forEach(function(item) {
+                dataNegara += '<tr>'
+                dataNegara += '<td id="text-left">'+ item.countryRegion + '</td>'
+                dataNegara += '<td>'+ item.confirmed + '</td>'
+                dataNegara += '<td>'+ item.recovered + '</td>'
+                dataNegara += '<td>'+ item.deaths + '</td>'
+                dataNegara += '</tr>'
+            })
 
-//     fetch('https://covid-19.mathdro.id/api/confirmed')
-//     .then(response2 => response2.json())
-//     .then(data2 => {
-//         console.log(data2)
+            document.getElementById("table").innerHTML = dataNegara
+        })
+        .catch(err => {
+            console.log(err)
+        });
+    }
 
-//         data2.forEach(function(item) {
-//             `<div class="negara">
-
-//                 <h2 class="judul"> Negara : ${item.countryRegion.value}</h2>
-
-//                 <div class="data-covid"> 
-
-//                     <div class="kolom">
-//                         <div class="red"> 
-//                         <div class="cardheading"> 
-//                             Positif : <br>
-//                             <span> ${item.confirmed} </span> <br>
-//                         </div></div>
-//                     </div>
-            
-//                     <div class="kolom">
-//                         <div class="green"> 
-//                             Sembuh : <br>
-//                             <span> ${item.recovered} </span>
-//                         </div>
-//                     </div>
-            
-//                     <div class= "kolom">
-//                         <div class="ungu"> 
-//                             Meninggal : <br>
-//                             <span> ${item.deaths} </span>
-//                         </div>
-//                     </div>       
-//                 </div>
-//             </div>`;
-//             // document.getElementById(".data-container").innerHTML = `${response2[0].item.uid.value}`
-          
-//         })
-
-//         document.getElementById("data-container").innerHTML = dataCari
+//============================================================
+// function getDataNegara() {
+ 
+//     let dataNegara = "<tr><th>Provinsi</th><th>Positif</th><th>Sembuh</th><th>Meninggal</th></tr>";
+ 
+//     fetch(`https://covid19.mathdro.id/api/countries/${negara}`)
+//     .then(response4 => response4.json())
+//     .then(data4 => {
+//             dataNegara += '<tr>'
+//             dataNegara += '<td>'+ negara.data4[0].countryRegion + '</td>'
+//             dataNegara += '<td>'+ data4.data4[0].confirmed + '</td>'
+//             dataNegara += '<td>'+ data4.data4[0].recovered + '</td>'
+//             dataNegara += '<td>'+ data4.data4[0].deaths + '</td>'
+//             dataNegara += '</tr>'
+ 
+//         document.getElementById("table").innerHTML = dataNegara
 //     })
-//     .catch(err2 => {
-//         console.log(err2)
+
+//     .catch(err => {
+//         console.log(err)
 //     });
 // }
-
-//=============cari==============
